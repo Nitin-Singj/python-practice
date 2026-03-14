4f27afa43f4d6ab6dd26e132088e7b6e3a5ccccc
Nitin-Singj <nmamta019@gmail.com>
2026-03-14 20:16:09 +0530

    Add files via upload
    Added python practice file

---
+++ chapter7.py
@@ -0,0 +1,58 @@
+# LOOPS
+
+    # WHILE LOOPS(jab tak condition false ni ho jata )
+i=1
+while(i<9): 
+    print(i)
+    i +=1
+# list using while 
+
+list= ["harry" , "nitin" , "neha"]
+i= 0
+while(i<len(list)):
+    print(list[i])
+    i +=1
+# FOR LOOPS
+for i in range(1,10):
+    print(i)
+for i in range(1,10,4):
+    print(i)
+ # for loop iterate 
+  # list 
+l = [1,2,578,3683]
+for i in l:
+ print(i)  
+ # tupple 
+ t=(35,468,448)
+ for i in t:
+    print(i)
+    # strings 
+    s=("nitin")
+    for i in s:
+        print(i)
+     # for loop with else 
+l=[345,37,38]
+for i in l:
+    print(i)
+else:
+    print("done")
+# break & continue 
+for i in range (50):
+    if (i==49):
+        break # exit the loop right now 
+    print(i) 
+
+for i in range (50):
+    if (i== 19):
+        continue # SKIP THE ITERATION 
+    print(i) 
+    # pass statement 
+for i in range (65):
+     pass
+i = 0 
+while (i<45): 
+ print(i)
+ i+= 2
+
+   
+
