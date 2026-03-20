2c1266dad0a3e5a6e2a6f67e8be129bb8b6fe98c
Nitin-Singj <nmamta019@gmail.com>
2026-03-20 13:08:04 +0530

    Add files via upload

---
+++ chapter 8.py
@@ -0,0 +1,28 @@
+# functions and recursions 
+def func1():        
+ print("hello world")
+func1()
+
+# function definition
+def avg():
+    a = int(input("enter your number "))
+    b = int(input("enter your number "))
+    c = int(input("ente r your number "))
+    average = (a + b+ c)/3
+    print(average)
+
+avg() #function call 
+print("thanks baby ")
+avg()
+  # function is user defined function
+ # FUNCTION WITH ARGUMENTS 
+def goodday(name):
+    print("good day",name)
+goodday("nitin") 
+# if we want to add some ending 
+def goodday(name, ending):
+    print("good day",name)
+    print(ending)
+goodday("nitin","thank you")
+# RECUSRSION (VERY IMPORTANT) EK AISA FUNCTION  JO APNE AP KO CALL KRTA HAI 
+   
